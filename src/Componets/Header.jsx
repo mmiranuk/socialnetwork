265a7743caed09b627102a79c541fb12ef8917bb
mmiraniuk <mmiranuk@gmail.com>
2021-07-16 17:11:14 +0300

    16072021

---
+++ src/Componets/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header=() =>{
+    return(
+        <header className='header'>
+        <img src='https://alcotrade.by/upload/css/logo.png'></img>
+      </header>
+    );
+}
+export default Header;
---
+++ src/Componets/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://99px.ru/sstorage/53/2013/12/tmb_91205_3821.jpg'></img>
+      </div>
+      <div>
+        ava and description
+      </div>
+      <div>
+        my posts
+        <div>
+          new post
+        </div>
+      </div>
+      <div>
+        <div>
+          Post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+export default Profile;
